ac72e3592a7495d2b6fe6edafe428e812f620870
hosainahmeed <skyfal430@gmail.com>
2026-01-17 12:07:48 +0600

    update

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import createMiddleware from "next-intl/middleware";
+import { locales } from "./config";
+
+export default createMiddleware({
+  locales,
+  defaultLocale: 'en'
+})
+
+export const config = {
+  matcher: [
+    "/",
+    "/(fr|en)/:path*",
+  ],
+}
